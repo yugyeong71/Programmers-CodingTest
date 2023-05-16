50377e40e15571ea8584ddd949b0a4a1ba175d8d
yugyeong71 <youkyoung71@naver.com>
2023-05-16 20:46:57 +0900

    Lv 2. 멀리 뛰기

---
+++ src/main/java/org/example/Lv2/LongJump.java
@@ -0,0 +1,21 @@
+package org.example.Lv2;
+
+/*
+    Lv 2. 멀리 뛰기
+*/
+public class LongJump {
+
+    public long solution(int n) {
+        long[] answer = new long[2001];
+
+        answer[1] = 1;
+        answer[2] = 2;
+
+        for (int i = 3; i < 2001; i++){
+            answer[i] = (answer[i -1 ]+ answer[i - 2]) % 1234567;
+        }
+
+        return answer[n];
+    }
+
+}
