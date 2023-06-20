7000db42f60725f499e96dfe7796072b07fa4bc5
yugyeong71 <youkyoung71@naver.com>
2023-06-20 23:18:47 +0900

    Lv 1. 약수의 합

---
+++ src/main/java/org/example/Lv1/DivisorSum.java
@@ -0,0 +1,20 @@
+package org.example.Lv1;
+
+/*
+    Lv 1. 약수의 합
+*/
+public class DivisorSum {
+
+    public int solution(int n) {
+        int answer = 0;
+
+        for (int i = 1; i <= n; i++) {
+            if (n % i == 0) {
+                answer += i;
+            }
+        }
+
+        return answer;
+    }
+
+}
